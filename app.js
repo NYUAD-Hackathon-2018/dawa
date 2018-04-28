6924cf190c93fd70da8904c10201ba33e6906106
abdullahzameek <35721712+abdullahzameek@users.noreply.github.com>
2018-04-28 11:12:47 +0400

    updated stuff

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cons = require('consolidate');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+
+app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
