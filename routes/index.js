2c90d40ce8ccb53faa715e3f5826a2d54408b1f2
abdullahzameek <35721712+abdullahzameek@users.noreply.github.com>
2018-04-29 12:22:53 +0400

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Simon Says So' });
+  res.render('index', { title: 'Block' });
 });
 
 module.exports = router;
